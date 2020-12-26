5aa9f247dacea8e3dcf9f6c0713c405b505d498e
Sedki-Sghairi <sedkisghairi3@gmail.com>
2020-12-26 22:43:39 +0400

    react-native-component View

--- App.js
+++ App.js
@@ -1,20 +1,18 @@
 import React, { useState } from 'react';
-import { Button, StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
-  [text, setText] = useState('Open up App.js to start working on your app!')
+  const [text, setText] = useState('Open up App.js to start working on your app!')
   return (
-    <View style={styles.container}>
-      <Text>{text}</Text>
-      <Button title='change text' onPress={() => setText('mrigel!')}/>
+    <View style={{padding:70}}>
+      <View>
+        <TextInput style={{placeholder: 'course goal'}}/>
+        <Button title='Add' />
+      </View>
+      <View>
+
+      </View>
     </View>
   );
 }
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
