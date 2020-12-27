0a833e50c2d8e4d6843f2583ed345f07b5e2f674
Sedki-Sghairi <sedkisghairi3@gmail.com>
2020-12-27 12:46:25 +0400

    react-native properties

--- App.js
+++ App.js
@@ -2,17 +2,41 @@ import React, { useState } from 'react';
 import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
-  const [text, setText] = useState('Open up App.js to start working on your app!')
-  return (
-    <View style={{padding:70}}>
-      <View>
-        <TextInput style={{placeholder: 'course goal'}}/>
-        <Button title='Add' />
-      </View>
-      <View>
-
-      </View>
-    </View>
-  );
+	const [ text, setText ] = useState('');
+	const [ goals, setGoals ] = useState([]);
+	const handleChange = (text) => {
+		setText(text);
+	};
+	const handleClick = () => {
+		setGoals((currentGoals) => [ ...currentGoals, text ]);
+	};
+	return (
+		<View>
+			<View style={styles.screen}>
+				<TextInput
+					placeholder="mrigel"
+					style={styles.inputContainer}
+					onChangeText={handleChange}
+					value={text}
+				/>
+				<Button title="Add" onPress={handleClick} />
+			</View>
+			<View>{goals.map((goal) => <Text>{goal}</Text>)}</View>
+		</View>
+	);
 }
 
+const styles = StyleSheet.create({
+	screen: {
+		padding: 70,
+		flexDirection: 'row',
+		justifyContent: 'space-between',
+		alignItems: 'center'
+	},
+	inputContainer: {
+		borderColor: 'grey',
+		borderWidth: 1,
+		width: ' 80%',
+		padding: 4
+	}
+});
